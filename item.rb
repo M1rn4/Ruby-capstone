1f71916a9d2d7b304f4322cc69f6be84c53ddfe0
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-05-02 16:43:40 -0600

    Create can_be_archive? method

--- item.rb
+++ item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item 
   
   def initialize(id,genre,author,source,label,date,archive)
@@ -11,7 +13,16 @@ class Item
   end
   
   def can_be_archive?
-
+    now = Date.today
+    before = Date.parse(@publish_date)
+    difference_in_days = (now - before).to_i
+    
+    years=(difference_in_days/365.25).to_i
+    if (years>10)
+      @archive = true
+    else
+      @archive = false  
+    end
   end
 
   def move_to_archive
