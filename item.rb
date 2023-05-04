384f82bab452a3e1ed2ea58879edcd0602b32583
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-05-04 09:14:29 -0600

    Modify initialize and can_be_archive

--- item.rb
+++ item.rb
@@ -1,23 +1,22 @@
 require 'date'
 
 class Item
-  def initialize(id, **options)
-    @id = id
-    @genre = options[genre]
-    @author = options[author]
-    @source = options[source]
-    @label = options[label]
-    @publish_date = options[date]
-    @archive = options[archive]
+  def initialize(**options)
+    @id = options['id']
+    @genre = options['genre']
+    @author = options['author']
+    @source = options['source']
+    @label = options['label']
+    @publish_date = options['publish_date']
+    @archive = options['archive']
   end
 
   def can_be_archive?
+    year_days = 365.25
     now = Date.today
     before = Date.parse(@publish_date)
     difference_in_days = (now - before).to_i
-
-    years = (difference_in_days / 365.25).to_i
-    years > 10
+    difference_in_days > 10 * year_days
   end
 
   def move_to_archive
