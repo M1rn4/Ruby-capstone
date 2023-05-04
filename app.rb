0f1df33a6f5889265658ecdda3562fae150e9fd1
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-05-04 14:50:15 -0600

    Create App class

---
+++ app.rb
@@ -0,0 +1,30 @@
+class App
+
+  def initialize
+    @books = []
+    @music_album = []
+    @movie = []
+    @games = []
+  end
+
+  def run_option(selected)
+    options = {
+      1 => :list_all_books,
+      2 => :list__all_music,
+      3 => :list_all_games,
+      4 => :List_all_genres,
+      5 => :List_all_labels,
+      6 => :List_all_authors,
+      7 => :Add_book,
+      8 => :Add_music_album,
+      9 => :Add_game,
+    }
+    self.send(options[selected])
+  end
+
+  def list_all_games
+    puts 'call list_all_games'
+  end
+
+
+end
--- main.rb
+++ main.rb
@@ -1,20 +1,30 @@
 require './menu'
 require './game'
 require './author'
+require './app'
 class Main
-  def initialize
+  def initialize    
+    @app= App.new
     puts 'Welcome to our catalog!'
   end
 
   def run
+    
     Menu.new
     selected = gets.chomp.to_i
     puts ''
     puts "You choose option #{selected}"
-    return unless selected != 13
+    
+    if selected != 10
+      @app.run_option(selected)
+    else
+      return
+    end
 
     run
   end
+
+
 end
 
 main = Main.new
